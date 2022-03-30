fef679f759f34777abcaeb4ea686d3d2eddc2919
Ryan Smillie <RyanJSmillie@hotmail.co.uk>
2022-03-30 20:48:55 +0100

    Kata1fizzBuzz

--- src/kata1.fizzBuzz.js
+++ src/kata1.fizzBuzz.js
@@ -1,3 +1,15 @@
-const fizzBuzz = number => {};
+const fizzBuzz = number => {
+    result = ""
+    if (number % 3 === 0 && number % 5 === 0) {
+        result = "FizzBuzz"
+    } else if (number % 3 === 0) {
+        result = "Fizz"
+    } else if (number % 5 === 0) {
+        result = "Buzz"
+    } else {
+        result = number;
+    }
+    return result;
+};
 
 module.exports = fizzBuzz;
